12d37f055f8ee5d2fa3eb8c4d6a9a5b7b28f3164
Anthony Stevens <aastevens1126@gmail.com>
2022-12-28 16:44:00 -0600

    get past seven day price history function update

--- src/main/java/com/example/networth/services/FinanceService.java
+++ src/main/java/com/example/networth/services/FinanceService.java
@@ -4,6 +4,7 @@ import com.example.networth.models.Asset;
 import com.example.networth.models.Portfolio;
 import com.example.networth.models.PortfolioAsset;
 import com.example.networth.repositories.PortfolioAssetRepository;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -13,7 +14,11 @@ import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class FinanceService {
@@ -21,7 +26,93 @@ public class FinanceService {
     @Autowired
     PortfolioAssetRepository paDao;
 
+    public JSONObject getJsonObject(String obj) throws ParseException {
+        JSONParser jsonParser = new JSONParser();
 
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(obj);
+
+        return jsonObject;
+    }
+
+    public JSONArray getJsonArray(String obj) throws ParseException {
+        JSONParser jsonParser = new JSONParser();
+
+        JSONArray jsonArray = (JSONArray) jsonParser.parse(obj);
+
+        return jsonArray;
+    }
+
+
+    /**************Functions for past data**************/
+    public void getSevenDayPrice() throws ParseException {
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject("https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=7&interval=daily", String.class);
+
+        Map<Long,Double>dayTotalPrice = new HashMap<>();
+
+        JSONObject obj = getJsonObject(result);
+        JSONArray days = getJsonArray(obj.get("prices").toString());
+
+        JSONArray day = getJsonArray(days.get(0).toString());
+
+        for(Object currentDay: days){
+            JSONArray dayArray = (JSONArray) currentDay;
+            Long unixTime = (Long) dayArray.get(0);
+            Double coinPrice = (Double) dayArray.get(1);
+
+            System.out.println( dayArray.get(0));
+//            System.out.println(dayArray.get(1));
+            dayTotalPrice.put(unixTime,coinPrice);
+        }
+
+        //Get previous price
+            //Day of price action(iterate through days)
+                //Get price for that day (API)(done)
+                //Use getTotalPrice function
+
+        //Get portfolio total amount for each day
+            //past seven day
+            //Creat map<day,portfolioTotal>
+                //iterate each day to get portfolio total
+                //PUT day and total in map
+        //return map
+
+
+    }
+
+    //Function return asset price on passed in UNIX timestamp (date parameter)
+    public double assetPriceForDay(String coinName, String date) throws ParseException {
+        coinName = coinName.toLowerCase();
+
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject("https://api.coingecko.com/api/v3/coins/"+coinName+"/history?date="+date,String.class);
+
+        JSONObject resultObj = getJsonObject(result);
+        JSONObject marketData = getJsonObject(resultObj.get("market_data").toString());
+        JSONObject currentPrice = getJsonObject(marketData.get("current_price").toString());
+//        System.out.println(currentPrice.get("usd"));
+
+        return (double) currentPrice.get("usd");
+
+    }
+
+    //Function Convert Unix Time regular date
+    public String getDate(long unixTime){
+
+        //convert seconds to milliseconds
+        Date date = new Date(unixTime);
+
+        // format of the date
+        SimpleDateFormat jdf = new SimpleDateFormat("dd-MM-yyyy");
+
+        String java_date = jdf.format(date);
+        System.out.println("\n"+java_date+"\n");
+
+        return java_date;
+    }
+
+    /**************Functions for current data**************/
+    //Function gets asset current price
     public double getPrice(String coinName) throws ParseException {
         coinName = coinName.toLowerCase();
 
@@ -41,6 +132,7 @@ public class FinanceService {
 
     }
 
+    //Function gets current portfolio total amount
     public double getTotalPrice(Portfolio selectedPortfolio) throws ParseException {
         //Total price of all assets
         double totalPrice = 0;
@@ -48,18 +140,18 @@ public class FinanceService {
         List<PortfolioAsset> portfolioAssets = paDao.findAllAssetsByPortfolio(selectedPortfolio);
         for (PortfolioAsset portfolioAsset: portfolioAssets){
 
-            System.out.println(portfolioAsset.getAsset().getName());
+//            System.out.println(portfolioAsset.getAsset().getName());
             //Get Current price
             double currentPrice = getPrice(portfolioAsset.getAsset().getName());
-            System.out.println(currentPrice);
+//            System.out.println(currentPrice);
 
             //Get quantity
             double quantity = portfolioAsset.getQuantity();
-            System.out.println(quantity);
+//            System.out.println(quantity);
 
             //Get total
             double assetTotal = currentPrice*quantity;
-            System.out.println(assetTotal);
+//            System.out.println(assetTotal);
             totalPrice += assetTotal;
         }
         totalPrice = new BigDecimal(totalPrice).setScale(2, RoundingMode.HALF_UP).doubleValue();
