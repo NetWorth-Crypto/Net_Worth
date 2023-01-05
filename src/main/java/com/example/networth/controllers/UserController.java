54eb969489136c1130d84c3ec52646c4d176513c
Anthony Stevens <aastevens1126@gmail.com>
2023-01-05 15:19:54 -0600

    profile image upload good

--- src/main/java/com/example/networth/controllers/UserController.java
+++ src/main/java/com/example/networth/controllers/UserController.java
@@ -8,13 +8,11 @@ import com.example.networth.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -33,6 +31,7 @@ public class UserController {
     @Autowired
     private FollowingRepository followingDao;
 
+    @Autowired
     private UserRepository userDao;
 
 
@@ -239,7 +238,29 @@ attributes.addFlashAttribute("success","You successfully registered! You can now
 
 
 
+    //Upload User Profile Image
+    @GetMapping ("/users/imageUpload/{baseImgUrl}/{extensionUrl}/{returnUrl}")
+    public String profileImageUpload(@PathVariable String baseImgUrl,
+                                     @PathVariable String extensionUrl,
+                                     @PathVariable String returnUrl){
+        System.out.println("Upload image controller hit");
+        System.out.println("base image url: "+baseImgUrl);
+        System.out.println("extension url: "+ extensionUrl);
+        System.out.println("return url: "+ returnUrl);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth.getPrincipal() == "anonymousUser")
+        {
+            return "redirect:login";
+        }
+        User loggedinUser =(User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userDao.getReferenceById(loggedinUser.getId());
 
+        String imgUrl = "https://"+baseImgUrl+"/"+extensionUrl;
+        user.setProfilePicture(imgUrl);
+        userDao.save(user);
+        System.out.println("Save should hit");
+        return "redirect:/"+returnUrl;
+    }
 
 
 
