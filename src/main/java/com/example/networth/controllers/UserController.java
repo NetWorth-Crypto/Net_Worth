a6968d853ddccc7cd91db5f68a64a4038fd3a7c0
Anthony Stevens <aastevens1126@gmail.com>
2023-01-05 15:19:54 -0600

    profile image upload good

--- src/main/java/com/example/networth/controllers/UserController.java
+++ src/main/java/com/example/networth/controllers/UserController.java
@@ -7,6 +7,7 @@ import com.example.networth.services.FollowingService;
 import com.example.networth.services.UserService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -32,6 +33,7 @@ public class UserController {
 
     private final FollowingService followingService;
 
+    @Autowired
     private UserRepository userDao;
 
 
@@ -258,7 +260,29 @@ public class UserController {
 
 
 
+    //Upload User Profile Image
+    @GetMapping ("/users/imageUpload/{baseImgUrl}/{extensionUrl}/{returnUrl}")
+    public String profileImageUpload(@PathVariable String baseImgUrl,
+                                     @PathVariable String extensionUrl,
+                                     @PathVariable String returnUrl){
+        System.out.println("Upload image controller hit");
+        System.out.println("base image url: "+baseImgUrl);
+        System.out.println("extension url: "+ extensionUrl);
+        System.out.println("return url: "+ returnUrl);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth.getPrincipal() == "anonymousUser")
+        {
+            return "redirect:login";
+        }
+        User loggedinUser =(User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userDao.getReferenceById(loggedinUser.getId());
 
+        String imgUrl = "https://"+baseImgUrl+"/"+extensionUrl;
+        user.setProfilePicture(imgUrl);
+        userDao.save(user);
+        System.out.println("Save should hit");
+        return "redirect:/"+returnUrl;
+    }
 
 
 
