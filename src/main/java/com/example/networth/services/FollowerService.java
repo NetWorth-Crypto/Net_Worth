4233b361e4b0c84048b9fe0ee18e80532dc00778
midoostar1 <95588511+midoostar1@users.noreply.github.com>
2023-01-06 10:00:52 -0600

    stashing to my branch

--- src/main/java/com/example/networth/services/FollowerService.java
+++ src/main/java/com/example/networth/services/FollowerService.java
@@ -1,66 +1,9 @@
-//package com.example.networth.services;
-//
-//import com.example.networth.models.Follower;
-//import com.example.networth.models.User;
-//import com.example.networth.repositories.FollowerRepository;
-//
-//import java.util.List;
-//
-//public class FollowerService {
-//    private final FollowerRepository followerDao;
-//    private final UserService userDao;
-//
-//    public FollowerService(FollowerRepository followerDao, UserService userDao) {
-//        this.followerDao = followerDao;
-//
-//        this.userDao = userDao;
-//    }
-//
-//
-//    public void removeFollower(long userId, long followerId){
-//        User user  = userDao.findById(userId);
-//        user.getFollowers().removeIf(x -> x.getId()==followerId);
-//        userDao.saveUser(user);
-//    }
-//
-//
-//    public List<Follower> getUserFollowers(User user){
-//        return user.getFollowers();
-//    }
-//
-//
-//    public Object findAll() {
-//        return followerDao.findAll();
-//    }
-//
-//    public List<Follower> getUserNotFollowers(User user){
-//        return followerDao.getUserNotFollowers(user.getId());
-//    }
-//
-//    public void save(Follower follower) {
-//        followerDao.save(follower);
-//    }
-//
-//    public Follower findById(long id) {
-//        return followerDao.findById(id).get();
-//    }
-//
-//    public void delete(Follower follower) {
-//        followerDao.delete(follower);
-//    }
-//
-//
-//
-//
-//}
-
-
 package com.example.networth.services;
 
 import com.example.networth.models.Follower;
-import com.example.networth.models.Post;
 import com.example.networth.models.User;
 import com.example.networth.repositories.FollowerRepository;
+
 
 //import com.example.networth.repositories.FollowingRepository;
 import com.example.networth.repositories.FollowingRepository;
@@ -75,6 +18,7 @@ import java.util.List;
 
 
 @Service
+
 public class FollowerService {
     private final FollowerRepository followerDao;
     private final UserService userDao;
@@ -102,9 +46,12 @@ public class FollowerService {
         return followerDao.findAll();
     }
 
+
 //    public List<Follower> getUserNotFollowers(User user){
 //        return followerDao.getUserNotFollowers(user.getId());
 //    }
+
+
 
     public void save(Follower follower) {
         followerDao.save(follower);
@@ -117,6 +64,9 @@ public class FollowerService {
     public void delete(Follower follower) {
         followerDao.delete(follower);
     }
+
+
+
 
 
 
