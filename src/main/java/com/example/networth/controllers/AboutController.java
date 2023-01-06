2b8b9606433a9d1b4e561c569e3188ea42cd9672
Casanova Geary <CasanovaGeary@Gmail.com>
2023-01-06 12:01:39 -0600

    Changing all navbars for each page and re-mapping.

--- src/main/java/com/example/networth/controllers/AboutController.java
+++ src/main/java/com/example/networth/controllers/AboutController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AboutController
 {
-    @GetMapping("/userProfile")
+    @GetMapping("/about")
     public String about()
     {
         return "about";
